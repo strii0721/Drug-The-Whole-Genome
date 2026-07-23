3fe8c92b61c6731989794d97b23eb7929674d20e
strii0721 <strii0721@outlook.com>
2026-07-23 15:34:56 +0800

    mod: 环境安装

---
+++ build_conda_env.sh
@@ -0,0 +1,2 @@
+conda env create -f environment.yaml
+git clone https://github.com/dptech-corp/Uni-Core.git /tmp/Uni-core && pip install --no-build-isolation /tmp/Uni-core
--- build_env.sh
+++
@@ -1,3 +0,0 @@
-conda env create -f conda/environment.yaml
-git clone https://github.com/dptech-corp/Uni-Core.git tmp/Uni-core
-cd tmp/Uni-core && pip install --no-build-isolation . && cd ..
